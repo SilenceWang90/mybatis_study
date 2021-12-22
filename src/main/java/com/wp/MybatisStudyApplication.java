996bc84f3b0a97305e37068f4eff38c27464c6ff
wangpeng116 <wangpeng116@longfor.com>
2021-12-22 15:57:36 +0800

    排除springboot数据源默认配置类

--- src/main/java/com/wp/MybatisStudyApplication.java
+++ src/main/java/com/wp/MybatisStudyApplication.java
@@ -2,8 +2,9 @@ package com.wp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MybatisStudyApplication {
     public static void main(String[] args) {
         SpringApplication.run(MybatisStudyApplication.class,args);
